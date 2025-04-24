45ff51407d63e937e1650b21e85d18e3ffabc9f7
erick-augusto <erick_aug@hotmail.com>
2025-04-24 19:15:27 -0300

    Refatorando o ednpoint de Pets

--- src/main/java/br/com/alura/adopet/api/repository/PetRepository.java
+++ src/main/java/br/com/alura/adopet/api/repository/PetRepository.java
@@ -1,8 +1,16 @@
 package br.com.alura.adopet.api.repository;
 
+import br.com.alura.adopet.api.model.Abrigo;
 import br.com.alura.adopet.api.model.Pet;
+
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
+    Collection<Pet> findAllByAdotadoFalse();
+
+    Collection<Pet> findByAbrigo(Abrigo abrigo);
 
 }
